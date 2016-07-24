5b326dbb0114a13789eec3f2ef6a2642823d5223
Jaon EarlWolf <jamonsterr@gmail.com>
2016-07-24 05:19:04 -0300

    remove entry.src and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 var webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/entry.js",
+  entry: './src/Sticky.js',
+  devtool: 'source-map',
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    path: __dirname + '/dist',
+    filename: 'sticky.js',
+    library: 'Sticky',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     loaders: [
