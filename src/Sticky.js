14311a177a2d1afa77570e1a8197449cb5a51e30
Jaon EarlWolf <jamonsterr@gmail.com>
2016-07-24 05:13:41 -0300

    add default blocks

--- src/Sticky.js
+++ src/Sticky.js
@@ -245,7 +245,7 @@ export default class Sticky {
   createBlock(name, data = {}) {
     const cfg = this.blocks[name] || this.__blocks[name];
 
-    if (!cfg) throw "Block not registered";
+    if (!cfg) throw `Block '${name}' not registered`;
 
     return Object.assign(new Brick(cfg), data);
   }
@@ -382,7 +382,69 @@ Sticky.prototype.__blocks = {
     title: '',
     icon: 'img/icon.png',
     behavior: () => 0
-  }
+  },
+  'Source': {
+    id: 'Source',
+    fill: '#4fec2f',
+    ports: { data_in: 0, data_out: 1, flow_in: 0, flow_out: 0 },
+    title: 'Source Block',
+    gui: {
+      number: { label: 'Number', type: 'number' }
+    },
+    behavior: function() {
+      return [this.inputs.number];
+    }
+  },
+  'Comparison': {
+    id: 'Comparison',
+    fill: '#4fec2f',
+    ports: { data_in: 2, data_out: 1, flow_in: 0, flow_out: 0 },
+    title: 'Comparison Block',
+    gui: {
+      op: { label: 'Operation', type: 'select', options: ['==', '!=', '===', '!==', '>', '>=', '<', '<='] },
+    },
+    behavior: function(findById) {
+      const conn1 = (this._ports['in'][0]._conn[0]);
+      const conn2 = (this._ports['in'][1]._conn[0]);
+      const brick1 = findById(conn1.brick);
+      const brick2 = findById(conn2.brick);
+      const val1 = brick1.behavior(findById)[conn1.id];
+      const val2 = brick2.behavior(findById)[conn2.id];
+
+      return [eval(`${val1} ${this.inputs.op} ${val2}`)];
+    }
+  },
+  'Alert': {
+    id: 'Alert',
+    fill: '#EC962F',
+    ports: { data_in: 1, data_out: 0, flow_in: 1, flow_out: 1 },
+    title: 'Alert Block',
+    behavior: function(findById) {
+      const conn = this._ports['in'][0]._conn[0];
+      const brick = findById(conn.brick);
+      const data = brick.behavior(findById)[conn.id];
+
+      alert(data);
+
+      return 0;
+    }
+  },
+  'Sum': {
+    id: 'Sum',
+    fill: '#3e67c2',
+    ports: { data_in: 2, data_out: 1, flow_in: 0, flow_out: 0 },
+    title: 'Sum Block',
+    behavior: function(findById) {
+      const conn1 = (this._ports['in'][0]._conn[0]);
+      const conn2 = (this._ports['in'][1]._conn[0]);
+      const brick1 = findById(conn1.brick);
+      const brick2 = findById(conn2.brick);
+      const val1 = brick.behavior(findById)[conn1.id];
+      const val2 = brick.behavior(findById)[conn2.id];
+
+      return [val1 + val2];
+    }
+  },
 };
 
 
--- src/entry.js
+++ src/entry.js
@@ -2,114 +2,25 @@ import Sticky from './Sticky.js';
 
 var canvas = new Sticky('test');
 
-canvas.registerBlock('Source', {
-  fill: '#4fec2f',
-  ports: { data_in: 0, data_out: 1, flow_in: 0, flow_out: 0 },
-  title: 'Source Block',
-  gui: {
-    // text: { label: 'Text', type: 'text' },
-    number: { label: 'Number', type: 'number' }
-  },
-  behavior: function() {
-    return [this.inputs.number];
-  }
-});
-
-canvas.registerBlock('Comparison', {
-  fill: '#4fec2f',
-  ports: { data_in: 2, data_out: 1, flow_in: 0, flow_out: 0 },
-  title: 'Comparison Block',
-  gui: {
-    op: { label: 'Operation', type: 'select', options: ['==', '!=', '===', '!==', '>', '>=', '<', '<='] },
-  },
-  behavior: function(findById) {
-    var conn1 = (this._ports['in'][0]._conn[0]);
-    var conn2 = (this._ports['in'][1]._conn[0]);
-    var brick1 = findById(conn1.brick);
-    var brick2 = findById(conn2.brick);
-    var val1 = brick1.behavior(findById)[conn1.id];
-    var val2 = brick2.behavior(findById)[conn2.id];
-
-    return [eval(`${val1} ${this.inputs.op} ${val2}`)];
-  }
-});
-
-canvas.registerBlock('Actuator', {
-  fill: '#673A7E',
-  ports: { data_in: 0, data_out: 0, flow_in: 1, flow_out: 1 },
-  title: 'Actuator Block',
-  behavior: `
-    console.log(this);
-    alert(this.value);
-    return 0;
-  `
-});
-
-canvas.registerBlock('Alert', {
-  fill: '#EC962F',
-  ports: { data_in: 1, data_out: 0, flow_in: 1, flow_out: 1 },
-  title: 'Alert Block',
-  behavior: function(findById) {
-    var conn = this._ports['in'][0]._conn[0];
-    console.log(conn, conn.brick)
-    var brick = findById(conn.brick);
-    var data = brick.behavior(findById)[conn.id];
-    alert(data);
-
-    return 0;
-  }
-});
-
-canvas.registerBlock('Sum', {
-  fill: '#3e67c2',
-  ports: { data_in: 2, data_out: 1, flow_in: 0, flow_out: 0 },
-  title: 'Sum Block',
-  behavior: function(findById) {
-    var val1 = (this._ports['in'][0]._conn[0]);
-    var val2 = (this._ports['in'][1]._conn[0]);
-    var brick = findById(val1.brick);
-    var data = brick.behavior(findById)[val2.id];
-    brick = findById(val2.brick);
-    data += brick.behavior(findById)[val2.id];
-
-    return [data];
-  }
-});
-
-
-let rect = canvas.createBlock('Actuator');
-let rect2 = canvas.createBlock('Actuator');
+let rect1 = canvas.createBlock('Sum');
+let rect2 = canvas.createBlock('Source');
 let rect3 = canvas.createBlock('Source');
 let rect4 = canvas.createBlock('Alert');
 let rect5 = canvas.createBlock('Sum');
-let rect6 = canvas.createBlock('Source');
-let rect7 = canvas.createBlock('Source');
-let rect8 = canvas.createBlock('Alert');
-let rect9 = canvas.createBlock('Sum');
-let rect10 = canvas.createBlock('Comparison');
+let rect6 = canvas.createBlock('Comparison');
 
-rect.x = 130; rect.y = 230;
+rect1.x = 130; rect1.y = 230;
 rect2.x = 330; rect2.y = 200;
 rect3.x = 330; rect3.y = 100;
 rect4.x = 130; rect4.y = 300;
 
-rect.value = 'helloooooo';
-rect2.value = 'world';
-rect3.value = 'nayn cat';
-rect6.value = [4];
-rect7.value = [2];
-
 window.canvas = canvas;
-// canvas.addObj(rect);
-// canvas.addObj(rect2);
-// canvas.addObj(rect3);
-// canvas.addObj(rect4);
+canvas.addObj(rect1);
+canvas.addObj(rect2);
+canvas.addObj(rect3);
+canvas.addObj(rect4);
 canvas.addObj(rect5);
 canvas.addObj(rect6);
-canvas.addObj(rect7);
-canvas.addObj(rect8);
-canvas.addObj(rect9);
-canvas.addObj(rect10);
 
 document.getElementById('run').onclick = function() {
   canvas.run();
