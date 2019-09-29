b55823fbf6c62b7a8732412423be9d057bfc27c2
Jaon <jamonsterr@gmail.com>
2019-09-28 22:54:14 -0300

    Fix error on umounting Line component

--- src/react/components/Line.js
+++ src/react/components/Line.js
@@ -1,30 +1,29 @@
 import LeaderLine from 'leader-line';
 import React from 'react';
 
+function shimPointAnchor (point) {
+  return point instanceof Element
+    ? LeaderLine.pointAnchor( point, { x: 0, y: 0 } )
+    : LeaderLine.pointAnchor( document.body, point )
+    ;
+}
+
 export default class Line extends React.Component {
   componentWillUnmount () {
+    const { svg } = this.line.getProps();
+    const { parentNode } = svg;
+
+    if (parentNode)
+      parentNode.replaceChild( this.mockSvg, svg );
+
     if (this.line)
       this.line.remove();
   }
 
-  shouldComponentUpdate() {
-    if (!this.line && this.mockRef && this.props.start && this.props.end) {
-      this.initLine();
-      return false;
-    }
-
-    if (this.line) {
-      this.line.position(false);
-    }
-
-    return false;
-  }
-
   initLine() {
     const { start, end } = this.props;
-
-    const startPoint =  LeaderLine.pointAnchor( start, { x: 0, y: 0 } );
-    const endPoint = LeaderLine.pointAnchor( end, { x: 0, y: 0 } );
+    const startPoint = shimPointAnchor(start);
+    const endPoint = shimPointAnchor(end);
 
     this.line = new LeaderLine({
       start: startPoint,
@@ -33,15 +32,41 @@ export default class Line extends React.Component {
       dash: true,
     });
 
-    const lineEl = this.line.getProps().svg;
-    const { parentNode } = this.mockRef;
-    parentNode.replaceChild(lineEl, this.mockRef);
+    const { svg } = this.line.getProps();
+    const { parentNode } = this.mockSvg;
+    svg.leaderLine = this.line;
+
+    if (parentNode)
+      parentNode.replaceChild( svg, this.mockSvg );
 
     if (typeof(this.props.onLoad) === 'function')
       this.props.onLoad(this.line);
   }
 
+  shouldComponentUpdate (nextProps) {
+    if (!this.line && this.mockSvg && this.props.start && this.props.end) {
+      this.initLine();
+      return false;
+    }
+
+    if (this.line) {
+
+      if (this.props.start != nextProps.start) {
+        this.line.start = shimPointAnchor(nextProps.start);
+      }
+
+      if (this.props.end != nextProps.end) {
+        this.line.end = shimPointAnchor(nextProps.end);
+      }
+
+      this.line.position(false);
+    }
+
+    return false;
+  }
+
+
   render() {
-    return <svg ref={ ref => this.mockRef = ref } />;
+    return <svg ref={ ref => this.mockSvg = ref } />;
   }
 }
