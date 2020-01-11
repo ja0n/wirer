b003fcae7e24fd33814f9f2e6df1e669d68fe65f
Jaon <jamonsterr@gmail.com>
2020-01-11 06:51:47 -0300

    Add react-uid to Connections key prop

--- src/react/components/Connections.js
+++ src/react/components/Connections.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { uid } from 'react-uid';
 import Line from './Line';
 import _get from 'lodash/get'
 
@@ -6,12 +7,12 @@ const Connections = ({ connections, canvas, onLoad }) => {
   if (canvas.render.internalRender)
     return null;
 
-  return connections.map((wire, index) => {
+  return connections.map((wire) => {
     const [sourcePort, targetPort] = wire.getControlPoints();
     if (sourcePort && targetPort)
       return (
         <Line
-          key={index}
+          key={uid(wire)}
           start={_get(sourcePort, '_el', sourcePort)}
           end={_get(targetPort, '_el', targetPort)}
           size={4 * canvas.render.zoom}
