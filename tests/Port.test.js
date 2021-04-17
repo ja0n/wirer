4e47ad9467b46dcd920cf9dd63da345035f2a666
Jaon <jaon-z@live.com>
2021-04-16 22:25:55 -0300

    add tests for attach method

--- tests/Port.test.js
+++ tests/Port.test.js
@@ -22,6 +22,101 @@ class SVGPolygonElement extends SVGElement {}
 window.SVGPolygonElement = SVGPolygonElement;
 
 describe('Port Class', () => {
+
+  test('attach', () => {
+    let canvas = null;
+    const wrapper = mount(
+      <Container
+        onLoad={ref => {
+          canvas = ref;
+        }}
+      />
+    );
+
+    const source = canvas.createNode('SourceString', { x: 10, y: 10 });
+    const operation = canvas.createNode('Operation', { x: 100, y: 100 });
+    canvas.addNodes([source, operation]);
+
+    // check if connection is empty
+    expect(source._ports.out[0].connections.length).toBe(0);
+    expect(operation._ports.in[0].connections.length).toBe(0);
+
+    // try invalid port
+    expect(
+      canvas.render.sealOrDiscard(source._ports.out[0], operation._ports.out[0])
+    ).toBeFalsy();
+
+    // check if connection is still empty
+    expect(source._ports.out[0].connections.length).toBe(0);
+    expect(operation._ports.in[0].connections.length).toBe(0);
+
+    // attach valid port
+    expect(
+      canvas.render.sealOrDiscard(source._ports.out[0], operation._ports.in[0])
+    ).toBeTruthy();
+
+    // check if connection is increased
+    expect(source._ports.out[0].connections.length).toBe(1);
+    expect(operation._ports.in[0].connections.length).toBe(1);
+
+    // attach valid port again
+    expect(
+      canvas.render.sealOrDiscard(source._ports.out[0], operation._ports.in[0])
+    ).toBeFalsy();
+
+    // check if connection didn't increase
+    expect(source._ports.out[0].connections.length).toBe(1);
+    expect(operation._ports.in[0].connections.length).toBe(1);
+
+  });
+
+  test('attach maxConnections', () => {
+    let canvas = null;
+    const wrapper = mount(
+      <Container
+        onLoad={ref => {
+          canvas = ref;
+        }}
+      />
+    );
+
+    const sourceA = canvas.createNode('SourceString', { x: 10, y: 10 });
+    const sourceB = canvas.createNode('SourceString', { x: 30, y: 30 });
+    const operation = canvas.createNode('Operation', { x: 100, y: 100 });
+    canvas.addNodes([sourceA, sourceB, operation]);
+
+    const operationPort = operation._ports.in[0];
+    const sourceAPort = sourceA._ports.out[0];
+    const sourceBPort = sourceB._ports.out[0];
+
+    // assert with 1
+    operationPort.maxConnections = 1;
+
+    expect(
+      canvas.render.sealOrDiscard(sourceAPort, operationPort)
+    ).toBeTruthy()
+    expect(sourceAPort.connections.length).toBe(1);
+    expect(operationPort.connections.length).toBe(1);
+    expect(sourceBPort.connections.length).toBe(0);
+
+    expect(
+      canvas.render.sealOrDiscard(sourceBPort, operationPort)
+    ).toBeFalsy()
+    expect(sourceAPort.connections.length).toBe(1);
+    expect(operationPort.connections.length).toBe(1);
+    expect(sourceBPort.connections.length).toBe(0);
+
+    // assert with 2
+    operationPort.maxConnections = 2;
+
+    expect(
+      canvas.render.sealOrDiscard(sourceBPort, operationPort)
+    ).toBeTruthy()
+    expect(sourceAPort.connections.length).toBe(1);
+    expect(operationPort.connections.length).toBe(2);
+    expect(sourceBPort.connections.length).toBe(1);
+  });
+
   test('dettach', () => {
     let canvas = null;
     const wrapper = mount(
