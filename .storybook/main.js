2e7ca21c5891fde3001e482b9dd8b4895caa401c
Jaon <jaon-z@live.com>
2021-05-08 01:09:12 -0300

    update storybook version and regex

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const custom = require('../webpack.config.js');
 
 module.exports = {
-  stories: ['../**/*.stories.js'],
+  stories: ['../stories/*.stories.js', '../src/**/*.stories.js'],
   webpackFinal: (config) => {
     config.module.rules.push(custom.module.rules[0])
     return config
