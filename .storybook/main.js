32455477bf7ade07d8b8e1e2bcfbb6ebdd47106f
Jaon <jaon-z@live.com>
2021-05-08 22:51:38 -0300

    update storybook to use webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,11 @@ const custom = require('../webpack.config.js');
 
 module.exports = {
   stories: ['../stories/*.stories.js', '../src/**/*.stories.js'],
+  core: {
+    builder: "webpack5",
+  },
   webpackFinal: (config) => {
-    config.module.rules.push(custom.module.rules[0])
-    return config
+    config.module.rules.push(custom.module.rules[0]);
+    return config;
   },
 };
