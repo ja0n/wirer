65f1967149b18808f9cf377f37367f08098eea87
Jaon <jaon-z@live.com>
2022-04-12 03:27:59 -0300

    add firmata nodes

---
+++ stories/browser-serialport.js
@@ -0,0 +1,123 @@
+const { EventEmitter } = require('events');
+const { Buffer } = require('buffer/')
+
+globalThis.Buffer = Buffer
+
+class SerialPort extends EventEmitter {
+  constructor(port, options) {
+    super(options);
+    this.options = options || {};
+
+    this.browser = true;
+    this.path = this.options.path;
+    this.isOpen = false;
+    this.port = null;
+    this.writer = null;
+    this.reader = null;
+    this.baudRate = this.options.baudRate;
+    this.requestOptions = this.options.requestOptions || {};
+
+    if (this.options.autoOpen) this.open();
+  }
+
+  static list(callback) {
+    return navigator.serial.getPorts()
+      .then((list) => {if (callback) {return callback(null, list)}})
+      .catch((error) => {if (callback) {return callback(error)}});
+  }
+
+  open(callback) {
+    window.navigator.serial.requestPort(this.requestOptions)
+      .then(serialPort => {
+        this.port = serialPort;
+        if (this.isOpen) return;
+        return this.port.open({ baudRate: this.baudRate || 57600 });
+      })
+      .then(() => this.writer = this.port.writable.getWriter())
+      .then(() => this.reader = this.port.readable.getReader())
+      .then(async () => {
+        this.emit('open');
+        this.isOpen = true;
+        if (callback) callback(null);
+        while (this.port.readable.locked) {
+          try {
+            const { value, done } = await this.reader.read();
+            if (done) {
+              break;
+            }
+            this.emit('data', Buffer.from(value));
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      })
+      .catch(error => {if (callback) {callback(error)}});
+  }
+
+  async close(callback) {
+    try {
+      await this.reader.releaseLock();
+      await this.writer.releaseLock();
+      await this.port.close();
+      this.isOpen = false;
+    } catch (error) {
+      if (callback) return callback(error);
+      throw error;
+    }
+    callback && callback(null);
+  }
+
+  async set(props = {}, callback) {
+    try {
+      const signals = {};
+      if (Object.prototype.hasOwnProperty.call(props, 'dtr')) {
+        signals.dataTerminalReady = props.dtr;
+      }
+      if (Object.prototype.hasOwnProperty.call(props, 'rts')) {
+        signals.requestToSend = props.rts;
+      }
+      if (Object.prototype.hasOwnProperty.call(props, 'brk')) {
+        signals.break = props.brk;
+      }
+      if (Object.keys(signals).length > 0) {
+        await this.port.setSignals(signals);
+      }
+    } catch (error) {
+      if (callback) return callback(error);
+      throw error;
+    }
+    if (callback) return callback(null);
+  }
+
+  write(buffer, callback) {
+    this.writer.write(buffer);
+    if (callback) return callback(null);
+  }
+
+  async read(callback) {
+    let buffer;
+    try {
+      buffer = await this.reader.read();
+    } catch (error) {
+      if (callback) return callback(error);
+      throw error;
+    }
+    if (callback) callback(null, buffer);
+  }
+
+  // TODO: is this correct?
+  flush(callback) {
+    //this.port.flush(); // is this sync or a promise?
+    console.warn('flush method is a NOP right now');
+    if (callback) return callback(null);
+  }
+
+  // TODO: is this correct?
+  drain(callback) {
+    // this.port.drain(); // is this sync or a promise?
+    console.warn('drain method is a NOP right now');
+    if (callback) return callback(null);
+  }
+}
+
+module.exports = SerialPort;
---
+++ stories/firmata.stories.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Container } from '../src/react/components';
+import firmataFlows from './flows/firmata-flows.json';
+const Firmata = require('firmata-io').Firmata;
+const SerialPort = require('./browser-serialport');
+
+const getRandom = (min, max) => min + (Math.random() * (max - min));
+
+const nodeConfig = {
+  'digitalWrite': {
+    fill: '#F5E867',
+    ports: { data_in: 2, data_out: 0, flow_in: 1, flow_out: 1 },
+    title: 'digitalWrite',
+    gui: {
+      port: { label: 'port', type: 'number' },
+      value: { label: 'value', type: 'select', options: [true, false] },
+    },
+    async behavior (findById, { board }) {
+      let port = this.inputs.port;
+      const portConn = this._ports['in'][0].connections[0];
+      const portNode = portConn && findById(portConn.nodeId);
+      if (portNode) {
+        port = portNode.behavior(findById)[portConn.id];
+      }
+
+      let value = JSON.parse(this.inputs.value);
+      const valueConn = this._ports['in'][1].connections[0];
+      const valueNode = valueConn && findById(valueConn.nodeId);
+      if (valueNode) {
+        value = (await valueNode.behavior(findById, { board }))[valueConn.id];
+      }
+
+      board.digitalWrite(port, value)
+      return 0;
+    }
+  },
+  'analogRead': {
+    fill: '#FF8D4F',
+    ports: { data_in: 1, data_out: 1, flow_in: 0, flow_out: 0 },
+    title: 'analogRead',
+    gui: {
+      port: { label: 'port', type: 'number' },
+    },
+    async behavior (findById, { board }) {
+      let port = this.inputs.port;
+      // const portConn = this._ports['in'][0].connections[0];
+      // const portNode = findById(portConn.nodeId);
+      // if (portNode) {
+      //   port = portNode.behavior(findById)[portConn.id];
+      // }
+
+      board.reportAnalogPin(port, 1)
+      return new Promise(resolve => {
+        const value = board.pins[board.analogPins[port]].value
+        console.log('analogRead', value)
+        resolve([value])
+        // board.analogRead(port, value => {
+        //   console.log('analogRead', value)
+        //   board.reportAnalogPin(port, 0)
+        //   resolve([value])
+        // })
+      })
+    }
+  },
+};
+
+storiesOf('Examples', module)
+  .add('Firmata', () => (
+    <div id="container">
+      <div id="test">
+        <div id="status"></div>
+        <Container
+          onLoad={canvas => {
+            const initialNodes = ['SourceNumber', 'SourceNumber', 'Alert', 'Operation', 'If', 'analogRead', 'digitalWrite', 'digitalWrite', 'delay', 'delay', 'analogRead'];
+            let board
+            globalThis.canvas = canvas
+            Object.keys(nodeConfig).forEach(key => canvas.registerNode(key, nodeConfig[key]));
+
+            document.getElementById('setup').onclick = function() {
+              if (!board) board = new Firmata(new SerialPort("/dev/ttyACM0", { baudRate: 57600, autoOpen: true }));
+
+              board.on("ready", () => {
+                // Arduino is ready to communicate
+                console.log('ready')
+                document.getElementById('run').removeAttribute('disabled')
+                // setInterval(() => {
+                //   pinValue = !pinValue
+                //   board.digitalWrite(13, pinValue)
+                //   document.querySelector('#status').innerHTML = pinValue.toString()
+                // }, 1000)
+              });
+            }
+
+            document.getElementById('run').onclick = function() {
+              canvas.run({ board });
+            };
+
+            document.getElementById('export').onclick = function() {
+              const json = canvas.toJSON();
+              navigator.clipboard.writeText(JSON.stringify(json, null, 2));
+            };
+
+            document.getElementById('random').onclick = function() {
+              randomNodes();
+            }
+
+            function loadExample (id) {
+              canvas.loadJSON(firmataFlows[id]);
+            };
+
+            function onLoad (event) {
+              loadExample(event.target.dataset.index);
+            }
+
+            for (let button of document.getElementsByClassName('load')) {
+              button.addEventListener('click', onLoad);
+            }
+
+            function randomNodes () {
+              const nodes = initialNodes.map(nodeName => {
+                const [x, y] = [getRandom(30, 500), getRandom(30, 300)];
+                return canvas.createNode(nodeName, { x, y });
+              });
+              canvas.clearCanvas();
+              canvas.addNodes(nodes);
+            }
+
+            window.setTimeout(() => {
+              // loadExample('0');
+            }, 0);
+          }}
+        />
+      </div>
+      <ul className="button-list">
+        <li><button id="setup" type="button">setup</button></li>
+        <li><button id="run" type="button" disabled>run</button></li>
+        <li><button id="random" type="button">random</button></li>
+        <li><button id="export" type="export">export</button></li>
+        <li className="separator"></li>
+        <li><button className="load" type="button" data-index="0">load 1</button></li>
+        <li><button className="load" type="button" data-index="1">load 2</button></li>
+        <li><button className="load" type="button" data-index="2">load 3</button></li>
+        <li><button className="load" type="button" data-index="3">load 4</button></li>
+        <li><button className="load" type="button" data-index="3">load 4</button></li>
+        <li><button className="load" type="button" data-index="4">load 5</button></li>
+      </ul>
+    </div>
+  ))
